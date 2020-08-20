d4784f7ce7ca7624c353252f8099924407b4f5cf
augtal <augustas.talacka@gmail.com>
2020-08-20 13:48:26 +0300

    Learned to save and load prediction model

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import pandas
 from sklearn.tree import DecisionTreeClassifier
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
+"""
 music_data = pandas.read_csv('music.csv')
 X = music_data.drop(columns=['genre'])
 y = music_data['genre']
+X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=0.2)
 
 model = DecisionTreeClassifier()
-model.fit(X, y)
-predictions = model.predict([ [21,1],[22,0] ])
+model.fit(X_train, y_train)
 
+joblib.dump(model, 'music-recomender.joblib')
+"""
+model = joblib.load('music-recomender.joblib')
+predictions = model.predict([[21,1]])
+
+"""
+predictions = model.predict(X_test)
+
+acc_Score = accuracy_score(y_test, predictions)
+print(acc_Score)
+"""
 print("Done")
